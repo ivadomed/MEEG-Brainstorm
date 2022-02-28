f29da7e040ce664ba1b6059c2edc930713818c93
AmbroiseOdonnat <ambroiseodonnattechnologie@gmail.com>
2022-02-27 22:34:04 -0500

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -3,7 +3,8 @@
 """
 This script is used to create train and test datasets and dataloaders.
 
-Usage: type "from get_dataloader import <function>" to use on of its functions.
+Usage: type "from model import <class>" to use one of its class.
+       type "from model import <function>" to use one of its function.
 
 Contributors: Ambroise Odonnat.
 """
@@ -16,78 +17,124 @@ from sklearn.model_selection import train_test_split
 from utils import get_class_distribution
 
 
-def train_test_dataset(allData, allLabels, train_size, shuffle, random_state):
+class Loader:
     
-    """
-    Split into train and test dataset.
-    
-    Args:
-        allData (array): Trials after CSP algorithm (n_trials)x(Nr)x(n_sample_points),
-        allLabels (array): Corresponding labels,
-        train_size (float): Proportion of dataset taken for training,
-        test_size (float): Proportion of dataset taken for testing,
-        shuffle (bool): Shuffle dataset before the split,
-        random_state (int): Insure reproductible shuffle.
+    def __init__(self, allData, allLabels, train_size,\
+                 batch_size, num_workers, random_state, shuffle, balanced):
         
-    Returns:
-        tuple: train_data (array): Trials after CSP algorithm (n_trials)x(Nr)x(n_sample_points) for training,
-               train_labels (array): Corresponding labels,
-               test_data (array): Trials after CSP algorithm (n_trials)x(Nr)x(n_sample_points) for testing,
-               test_labels (array): Corresponding labels.
-    """
+        """
+        Args:
+            allData (array): Trials after CSP algorithm (n_trials x Nr x n_sample_points),
+            allLabels (array): Corresponding labels,
+            train_size (float): Proportion of dataset taken for training,
+            batch_size (float): Size of batches,
+            num_workers (float): Number of loader worker processes,
+            random_state (int): Insure reproductible shuffle,
+            shuffle (bool): Shuffle dataset before the split,
+            balanced (bool): Apply weight classes to compensate unbalance between classes.
+        """
+            
+        self.allData = allData
+        self.allLabels = allLabels
+        self.train_size = train_size
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.random_state = random_state
+        self.shuffle = shuffle
+        self.balanced = balanced
+        
+        
+    def train_test_dataset(self, allData, allLabels, train_size, shuffle, random_state):
+
+        """
+        Split into train and test dataset.
+
+        Args:
+            allData (array): Trials after CSP algorithm (n_trials x Nr x n_sample_points),
+            allLabels (array): Corresponding labels,
+            train_size (float): Proportion of dataset taken for training,
+            shuffle (bool): Shuffle dataset before the split,
+            random_state (int): Insure reproductible shuffle.
+
+        Returns:
+            tuple: train_data (array): Trials after CSP algorithm (n_trials x Nr x n_sample_points) for training,
+                   train_labels (array): Corresponding labels,
+                   test_data (array): Trials after CSP algorithm (n_trials x Nr x n_sample_points) for testing,
+                   test_labels (array): Corresponding labels.
+        """
+
+        # Split train and test dataset
+        train_data, test_data, train_labels, test_labels = train_test_split(allData, allLabels,\
+                                                                            train_size = train_size,\
+                                                                            shuffle = shuffle,\
+                                                                            random_state = random_state)
     
-    # Split train and test dataset
-    train_data, test_data, train_labels, test_labels = train_test_split(allData, allLabels,\
-                                                                        train_size = train_size,\
-                                                                        shuffle = shuffle,\
-                                                                        random_state = random_state)
-    
-    return train_data, train_labels, test_data, test_labels
+        return train_data, train_labels, test_data, test_labels
 
 
-def get_dataloader(data, labels, batch_size, num_workers, balanced):
+    def get_dataloader(self, data, labels, batch_size, num_workers, balanced):
 
-    """
-    Get dataloader.
-    
-    Args:
-        data (array): Trials after CSP algorithm (n_trials)x(Nr)x(n_sample_points),
-        labels (array): Corresponding labels,
-        batch_size (float): Size of batches,
-        num_workers (float): Number of loader worker processes,
-        balanced (bool): Weight classes to compensate unbalance between classes.
-        
-    Returns:
-        tuple: train_data (array): Trials after CSP algorithm (n_trials)x(Nr)x(n_sample_points) for training,
-               train_labels (array): Corresponding labels,
-               test_data (array): Trials after CSP algorithm (n_trials)x(Nr)x(n_sample_points) for testing,
-               test_labels (array): Corresponding labels.
-    """
-    
-    # To correct unbalance between classes, during training for instance
-    if balanced:
-        
-        # Recover weighted sampler
-        count_label = get_class_distribution(data, labels, False)
-        data, labels = torch.from_numpy(data),torch.from_numpy(labels)    
-        class_count = [i for i in count_label.values()]
-        class_weights = 1./torch.tensor(class_count, dtype=torch.float) 
-        class_weights_all = class_weights[labels]
-        weighted_sampler = WeightedRandomSampler(weights=class_weights_all,num_samples=len(class_weights_all),replacement=True)
-        
-        # Get dataloader
-        dataset = TensorDataset(data, labels)
-        dataloader = DataLoader(dataset = dataset, batch_size = batch_size,\
-                                shuffle = False, num_workers = num_workers, sampler = weighted_sampler)
-    
-    # To shuffle trials during testing for instance
-    else:
-        
-        # Get dataloader
-        data, labels = torch.from_numpy(data),torch.from_numpy(labels)    
-        dataset = TensorDataset(data, labels)
-        dataloader = DataLoader(dataset = dataset, batch_size = batch_size,\
-                            shuffle = True, num_workers = num_workers)
+        """
+        Get dataloader.
 
-    return dataloader
+        Args:
+            data (array): Trials after CSP algorithm with expanded dimension of size (n_trials x 1 x Nr x n_sample_points),
+            labels (array): Corresponding labels,
+            batch_size (float): Size of batches,
+            num_workers (float): Number of loader worker processes,
+            balanced (bool): Apply weight classes to compensate unbalance between classes.
 
+        Returns:
+            tuple: dataloader (Dataloader): Dataloader of trials batches of size (batch_size x 1 x n_channels x n_sample_points).
+        """
+
+        # To correct unbalance between classes, during training for instance
+        if balanced:
+
+            # Recover weighted sampler
+            count_label = get_class_distribution(labels, False)
+            class_count = [count_label[k] for k in np.unique(labels)]
+            class_weights = 1./torch.tensor(class_count, dtype=torch.float)
+            data, labels = torch.from_numpy(data),torch.from_numpy(labels)              
+            class_weights_all = class_weights[labels]
+            weighted_sampler = WeightedRandomSampler(weights=class_weights_all,num_samples=len(class_weights_all),replacement=True)
+            
+            # Get dataloader
+            dataset = TensorDataset(data, labels)
+            dataloader = DataLoader(dataset = dataset, batch_size = batch_size,\
+                                    shuffle = False, num_workers = num_workers, sampler = weighted_sampler)
+
+        # To shuffle trials during testing for instance
+        else:
+
+            # Get dataloader
+            data, labels = torch.from_numpy(data),torch.from_numpy(labels)    
+            dataset = TensorDataset(data, labels)
+            dataloader = DataLoader(dataset = dataset, batch_size = batch_size,\
+                                shuffle = True, num_workers = num_workers)
+
+        return dataloader
+    
+    
+    def train_test_dataloader(self):
+        
+        """
+        Returns:
+            tuple: train_dataloader (Dataloader): Dataloader of trials batches of size (batch_size x 1 x n_channels x n_sample_points) for training,
+                   test_dataloader (Dataloader): Dataloader of trials batches of size (batch_size x 1 x n_channels x n_sample_points) for testing.
+        """
+                
+        # Split data and labels in train and test
+        data_train, labels_train, data_test, labels_test = self.train_test_dataset(self.allData, self.allLabels,\
+                                                                   self.train_size, self.shuffle, self.random_state)
+        
+        # Expand dimension to batch_size x 1 x n_channels x time points for convolution layers
+        data_train = np.expand_dims(data_train, axis=1)
+        data_test = np.expand_dims(data_test, axis = 1)
+
+        # Recover train and test dataloader
+        train_dataloader = self.get_dataloader(data_train, labels_train, self.batch_size, self.num_workers, self.balanced)
+        test_dataloader = self.get_dataloader(data_test, labels_test, self.batch_size, self.num_workers, balanced = False)
+        
+        return train_dataloader, test_dataloader
+        
--- utils.py
+++ utils.py
@@ -8,10 +8,13 @@ Usage: type "from utils import <function>" to use on of its functions.
 Contributors: Ambroise Odonnat.
 """
 
-import seaborn as sns
-import pandas as pd
+import torch
 
-def get_class_distribution(data, label, display):
+import pandas as pd
+import seaborn as sns
+
+
+def get_class_distribution(label, display):
 
     """
     Get proportion of each class in the dataset data.
@@ -25,14 +28,46 @@ def get_class_distribution(data, label, display):
             count_label (dictionnary): Keys are label and values are corresponding proportion.
     """
     
-    count_label = {k:0 for k in label }
+    count_label = {k:0 for k in label[::-1] }
     for k in label:
         count_label[k] += 1
+        
+    # Force dictionnary to be ordered by keys
+    count_label = dict(sorted(count_label.items(), key=lambda item: item[0]))
     
     # Plot distribution
     if display:
-        print("Distribution of classes: \n",count_label)
-        sns.barplot(data = pd.DataFrame.from_dict([count_label]).melt(), x = "variable", y="value",\
-                    hue="variable").set_title('Number of spikes distribution')
+        print("Class Distribution: \n",count_label)
+        sns.barplot(data = pd.DataFrame.from_dict([count_label]).melt(), x = "variable", y="value")\
+        .set_title('Class Distribution')
     
     return count_label
+
+
+def check_balance(dataloader, n_classes, n_epochs, display):
+     
+    """
+    Plot histogramm of class distribution to check balance in the dataloader.
+    
+    Args:
+        dataloader (Dataloader): Dataloader of training trials batches of size (batch_size x 1 x n_channels x n_sample_points),
+        n_classes (int): Number of classes,
+        display (bool): Display histogram of class repartition.
+        
+    Returns:
+            acc (dictionnary): Keys are label and values are corresponding proportion.
+    """
+    
+    acc = {k:0 for k in range(n_classes)}
+    N = len(dataloader.dataset)
+    for epoch in range(n_epochs):
+        for i, (data, labels) in enumerate(dataloader):
+            for k in acc.keys():
+                acc[k] += int(torch.sum(labels == k)) 
+                
+    # Plot distribution
+    if display:
+        print("Class Distribution: \n",acc)
+        sns.barplot(data = pd.DataFrame.from_dict([acc]).melt(), x = "variable", y="value",)\
+        .set_title("Class Distribution")
+    return acc
