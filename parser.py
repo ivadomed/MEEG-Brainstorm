d20a4fff051c1074905c5165e7e852136a8e4e86
AmbroiseOdonnat <ambroiseodonnattechnologie@gmail.com>
2022-03-28 11:06:45 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -30,8 +30,10 @@ def get_parser():
         description = 'Get command instruction and configuration file',
         add_help = True  
     )
+     
+    # COMMAND ARGUMENTS
 
-    command_group = parser.add_mutually_exclusive_group(required = False)
+    command_group = parser.add_mutually_exclusive_group(required = True)
 
     
     command_group.add_argument('--train', dest = 'train', action = 'store_true',
@@ -40,32 +42,40 @@ def get_parser():
     command_group.add_argument('--test', dest = 'test', action = 'store_true',
                                help = 'Perform testing on trained model.')
     
-    parser.add_argument('--save', dest = 'save', action = 'store_true',
-                               help = 'Save model and optimizer states as well as training configuration file.')
-
-    # OPTIONAL ARGUMENTS
-    optional_args = parser.add_argument_group('OPTIONAL ARGUMENTS')
-
-    optional_args.add_argument('-pd', '--path-data', dest = 'path_data', required = True, type = dir_path,
+    
+    # ARGUMENTS
+    
+    parser.add_argument('-pd', '--path-data', dest = 'path_data', required = True, type = dir_path,
                                help = 'Path to data in .mat format')
     
-    optional_args.add_argument('-pcd', '--path-config_data', dest = 'path_config_data', required = True, type = file_path,
+    parser.add_argument('-pcd', '--path-config_data', dest = 'path_config_data', required = True, type = file_path,
                                help = 'Path to configuration dictionnary for data.')   
     
-    optional_args.add_argument('-pct', '--path-config_training', dest = 'path_config_training', required = True, type = file_path,
+    parser.add_argument('-pct', '--path-config_training', dest = 'path_config_training', required = True, type = file_path,
                                help = 'Path to configuration dictionnary for training.')
     
-    optional_args.add_argument('-pm', '--path-model', dest = 'path_model', required = True, type = file_path,
+    parser.add_argument('-pm', '--path-model', dest = 'path_model', required = True, type = file_path,
                                help = 'Path to save model state.')
     
-    optional_args.add_argument('-po', '--path-optimizer', dest = 'path_optimizer', required = True, type = file_path,
-                               help = 'Path to save optimizer state.')
-    
-    optional_args.add_argument('-pc', '--path-config', dest = 'path_config', required = True, type = file_path,
+    parser.add_argument('-pc', '--path-config', dest = 'path_config', required = True, type = file_path,
                                help = 'Path to save training configuration file.')
     
-    optional_args.add_argument('-gpu', '--gpu_id', dest = 'gpu_id', required = False, type = int,
-                               help = 'Id of the wanted gpu device.')    
+    parser.add_argument('-gpu', '--gpu_id', dest = 'gpu_id', required = True, type = int,
+                               help = 'Id of the wanted gpu device.') 
+    
+
+    # OPTIONAL ARGUMENTS
+    
+    optional_args = parser.add_argument_group('OPTIONAL ARGUMENTS')
+   
+    optional_args.add_argument('--save', dest = 'save', action = 'store_true', required = False,
+                               help = 'Save model and optimizer states as well as training configuration file.')
+    
+    optional_args.add_argument('--scheduler', dest = 'scheduler', action = 'store_true', required = False,
+                               help = 'Use a scheduler on the learning rate.')
+    
+    optional_args.add_argument('--amsgrad', dest = 'amsgrad', action = 'store_true', required = False,
+                               help = 'Use AMSGrad instead of ADAM as optimizer algorithm.')
     
     optional_args.add_argument('-weight_decay', dest = 'weight_decay', required = False, type = float,
                                help = 'Weight_decay in optimizer for L2 regularization.')
@@ -87,52 +97,55 @@ def get_tune_parser():
         add_help = True  
     )
 
+    # COMMAND ARGUMENTS
+    
     command_group = parser.add_mutually_exclusive_group(required = False)
 
     
     command_group.add_argument('--tune', dest = 'tune', action = 'store_true',
                                help = 'Tune hyperparameters on model.')
+    
+    
 
-    # OPTIONAL ARGUMENTS
-    optional_args = parser.add_argument_group('OPTIONAL ARGUMENTS')
+    # ARGUMENTS
 
-    optional_args.add_argument('-pd', '--path-data', dest = 'path_data', required = True, type = dir_path,
+    parser.add_argument('-pd', '--path-data', dest = 'path_data', required = True, type = dir_path,
                                help = 'Path to data in .mat format')
     
-    optional_args.add_argument('-pcd', '--path-config_data', dest = 'path_config_data', required = True, type = file_path,
+    parser.add_argument('-pcd', '--path-config_data', dest = 'path_config_data', required = True, type = file_path,
                                help = 'Path to configuration dictionnary for data.')   
 
-    optional_args.add_argument('-ptc', '--path-tuning_config', dest = 'path_tuning_config', required = True, type = file_path,
+    parser.add_argument('-ptc', '--path-tuning_config', dest = 'path_tuning_config', required = True, type = file_path,
                                help = 'Path to tuning configuration dictionnary for hyperparameters tuning.') 
 
-    optional_args.add_argument('-r', '--path-results', dest = 'path_results', required = True, type = dir_path,
+    parser.add_argument('-r', '--path-results', dest = 'path_results', required = True, type = dir_path,
                                help = 'Path to folder results.')
     
-    optional_args.add_argument('-pbm', '--path-best_states', dest = 'path_best_states', required = True, type = file_path,
+    parser.add_argument('-pbm', '--path-best_states', dest = 'path_best_states', required = True, type = file_path,
                                help = 'Path to save best model and optimizer states.')
     
-    optional_args.add_argument('-pc', '--path-best_config', dest = 'path_best_config', required = True, type = file_path,
+    parser.add_argument('-pc', '--path-best_config', dest = 'path_best_config', required = True, type = file_path,
                                help = 'Path to save best model configuration file.')
 
-    optional_args.add_argument('-n', '--n_samples', dest = 'n_samples', required = False, type = int,
+    parser.add_argument('-n', '--n_samples', dest = 'n_samples', required = True, type = int,
                                help = 'Number of samples per trial during hyperparameters search.')
     
-    optional_args.add_argument('-me', '--max_n_epochs', dest = 'max_n_epochs', required = False, type = int,
+    parser.add_argument('-me', '--max_n_epochs', dest = 'max_n_epochs', required = True, type = int,
                                help = 'Maximum number of iterations per trial during hyperparameters search.')
     
-    optional_args.add_argument('-gpu', '--gpu_id', dest = 'gpu_id', required = False, type = int,
+    parser.add_argument('-gpu', '--gpu_id', dest = 'gpu_id', required = True, type = int,
                                help = 'Id of the wanted gpu device.')
     
-    optional_args.add_argument('-gpu_res', '--gpu_resources', dest = 'gpu_resources', required = False, type = int,
+    parser.add_argument('-gpu_res', '--gpu_resources', dest = 'gpu_resources', required = True, type = int,
                                help = 'Number of gpus to used per trial.')
     
-    optional_args.add_argument('-cpu_res', '--cpu_resources', dest = 'cpu_resources', required = False, type = int,
+    parser.add_argument('-cpu_res', '--cpu_resources', dest = 'cpu_resources', required = True, type = int,
                                help = 'Number of cpus to used per trial.')
     
-    optional_args.add_argument('-m', '--metric', dest = 'metric', required = False, type = str,
+    parser.add_argument('-m', '--metric', dest = 'metric', required = True, type = str,
                                help = 'Metric to tune hyperparameters in Ray tune.')
     
-    optional_args.add_argument('-mo', '--mode', dest = 'mode', required = False, type = str,
+    parser.add_argument('-mo', '--mode', dest = 'mode', required = True, type = str,
                                help = 'Mode to tune hyperparameters in Ray tune.')
     
     return parser
