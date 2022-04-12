7c3ddca00bc5b8405f99bf8ec1b58416712a46a3
AmbroiseOdonnat <ambroiseodonnattechnologie@gmail.com>
2022-04-12 18:24:41 -0400

    Apply PEP8 conventions

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
-#!/opt/anaconda3/bin/python
+#!/usr/bin/env python
 
 """
-This script is used to gather small Python functions.
+This script gathers small Python functions.
 
 Usage: type "from utils import <function>" to use on of its functions.
 
@@ -10,123 +10,97 @@ Contributors: Ambroise Odonnat.
 
 import torch
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 
-from torch.autograd import Variable
 from loguru import logger
+from torch.autograd import Variable
 
 
-def get_spike_events(spikeTimePoints, N, freq):
+def get_spike_events(spike_time_points, n_time_points, freq):
 
     """
-    Compute array with binary values of size the number of time points with 1 when a spike occurs and 0 elsewhere.
+    Compute array of dimension [n_time_points] with 1 when a spike occurs and 0 otherwise.
     
     Args:
-        spikeTimePoints (array): Array of spike times (if none, empty array),
-        N (int): Number of time points,
+        spike_time_points (array): Contains time points when a spike occurs if any.
+        n_time_points (int): Number of time points.
         freq (int): Sample frequence of the EEG/MEG signals.
         
     Returns:
-        spikeEvents (array): Array with dimension N with 1 when a spike occurs and 0 elsewhere.
+        spike_events (array): Array of dimension [n_time_points] containing 1 when a spike occurs and 0 otherwise. 
     """
     
-    spikeEvents = np.zeros(N)
-    for time in spikeTimePoints:
+    spike_events = np.zeros(n_time_points)
+    for time in spike_time_points:
         index = int(freq*time)
-        spikeEvents[index] = 1
+        spike_events[index] = 1
         
-    return spikeEvents.astype(int)
+    return spike_events.astype(int)
 
 
-def get_spike_windows(spikeEvents, n_windows):
+def get_spike_windows(spike_events, n_time_windows):
     
     """
-    Split a given array of dimension n_time_points into n_windows map each window on 0
-    if no spike occurs in the given time window and 1 otherwize.
+    Compute tensor of dimension [batch_size x n_time_windows] with 1 when a spike occurs in the time window and 0 otherwise.
     
     Args:
-        spikeEvents (tensor): Tensor ofdimension n_windows with value 1 if a spike occurs in the given window, 0 otherwise,
-        n_window (int): Number of time windows.
+        spike_events (tensor): Tensor of dimension [batch_size x n_time_points] 
+                               containing 1 when a spike occurs and 0 otherwise. 
+        n_time_windows (int): Number of time windows.
         
     Return:
-        tensor of dimension n_windows with value 1 if a spike occurs in the given window, 0 otherwise.
+        spike_windows (tensor): Tensor of dimension [batch_size x n_time_windows] 
+                                with 1 when a spike occurs in the time window and 0 otherwise
     """
     
-    # Split spikeEvents in n_windows time windows
-    batch_size = spikeEvents.size(0)
-    spikeWindows = np.zeros((n_windows,batch_size))
-    chunks = torch.chunk(spikeEvents,n_windows,dim = -1)
+    # Split spike_events in n_time_windows time windows
+    batch_size = spike_events.size(0)
+    spike_windows = np.zeros((n_time_windows,batch_size))
+    chunks = torch.chunk(spike_events, n_time_windows, dim=-1)
+    
+    # Put 1 when a spike occurs in the time window, 0 otherwise
     for i,chunk in enumerate(chunks):
-        l = (chunk.sum(dim = -1) > 0).int()
-        spikeWindows[i] = l
-    return torch.Tensor(spikeWindows).t()
+        is_spike = (chunk.sum(dim=-1) > 0).int()
+        spike_windows[i] = is_spike
+    spike_windows = torch.Tensor(spike_windows).t()
+    
+    return spike_windows
 
 
-def get_class_distribution(label, display = False, save = False, title = None):
+def get_class_distribution(labels, display=False, save=False, title=None):
 
     """
-    Get proportion of each class in the dataset data.
+    Plot the distribution of labels.
     
     Args:
-        data (array): Training trials after CSP algorithm (n_trials)x(Nr)x(n_sample_points),
-        label (array): Corresponding labels,
-        display (bool): Display histogram of class repartition,
-        save (bool): Save image,
-        title (str): name and format of saved file.
+        labels (array): Labels in the dataset.
+        display (bool): Display histogram of class repartition.
+        save (bool): Save image.
+        title (str): Name and format of saved file.
         
     Returns:
-            count_label (dictionnary): Keys are label and values are corresponding proportion.
+        count_labels (dictionnary): Keys are labels and values are the corresponding proportions.
     """
     
-    count_label = {k:0 for k in label[::-1] }
-    for k in label:
-        count_label[k] += 1
+    count_labels = {k:0 for k in labels[::-1] }
+    for k in labels:
+        count_labels[k] += 1
         
     # Force dictionnary to be ordered by keys
-    count_label = dict(sorted(count_label.items(), key=lambda item: item[0]))
+    count_labels = dict(sorted(count_labels.items(), key=lambda item: item[0]))
     
     # Plot distribution
     if display:
-        print("Class Distribution: \n",count_label)
-        sns.barplot(data = pd.DataFrame.from_dict([count_label]).melt(), x = "variable", y="value")\
-        .set_title('Class Distribution')
-    
+        print("Class Distribution: \n",count_labels)
+        sns.barplot(data = pd.DataFrame.from_dict([count_labels]).melt(),
+                    x="variable", y="value").set_title('Class Distribution')
     if save:
         plt.savefig(title) 
         
-    return count_label
-
-
-def check_balance(dataloader, n_classes, n_epochs, display):
-     
-    """
-    Plot histogramm of class distribution to check balance in the dataloader.
-    
-    Args:
-        dataloader (Dataloader): Dataloader of training trials batches of size (batch_size x 1 x n_channels x n_sample_points),
-        n_classes (int): Number of classes,
-        display (bool): Display histogram of class repartition.
-        
-    Returns:
-            acc (dictionnary): Keys are label and values are corresponding proportion.
-    """
-    
-    acc = {k:0 for k in range(n_classes)}
-    N = len(dataloader.dataset)
-    for epoch in range(n_epochs):
-        for i, (data, labels) in enumerate(dataloader):
-            for k in acc.keys():
-                acc[k] += int(torch.sum(labels == k)) 
-                
-    # Plot distribution
-    if display:
-        print("Class Distribution: \n",acc)
-        sns.barplot(data = pd.DataFrame.from_dict([acc]).melt(), x = "variable", y="value",)\
-        .set_title("Class Distribution")
-    return acc
+    return count_labels
 
 
 def define_device(gpu_id):
@@ -135,24 +109,25 @@ def define_device(gpu_id):
     Define the device used for the process of interest.
     Args:
         gpu_id (int): ID of the cuda device.
-                      Default: 0
+        
     Returns:
-        tuple : cuda_available (bool): True if cuda device is available.
-                device (str): Cuda device.
+        cuda_available (bool): If True, cuda is available.
+        device (str): Cuda device.
     """
     
     device = torch.device("cuda:" + str(gpu_id) if torch.cuda.is_available() else "cpu")
     cuda_available = torch.cuda.is_available()
     
-    if not cuda_available:
-        logger.info("Cuda is not available.")
-        logger.info("Working on {}.".format(device))
+    # Check cuda availability
     if cuda_available:
         
         # Set the GPU
         gpu_id = int(gpu_id)
         torch.cuda.set_device(gpu_id)
         logger.info(f"Using GPU ID {gpu_id}")
+    else:
+        logger.info("Cuda is not available.")
+        logger.info("Working on {}.".format(device))
         
     return cuda_available, device
 
@@ -189,7 +164,8 @@ def inverse_number_samples(labels):
     class_count = [count_label[k] for k in np.unique(labels)]
     
     # Compute the corresponding weights
-    class_weights = 1. / torch.tensor(class_count, dtype = torch.float)
+    class_weights = 1./torch.tensor(class_count, dtype=torch.float)
+    
     return class_weights
 
 
@@ -210,23 +186,21 @@ def inverse_square_root_number_samples(labels):
     class_count = [count_label[k] for k in np.unique(labels)]
     
     # Compute the corresponding weights
-    class_weights = 1. / np.power(torch.tensor(class_count, dtype = torch.float), 1 / 2) 
+    class_weights = 1./np.power(torch.tensor(class_count, dtype = torch.float), 1/2) 
+    
     return class_weights
 
 
-def effective_number_samples(labels, beta = 0.9):
+def effective_number_samples(labels, beta=0.9):
 
     """
-    Compute Effective Number of Samples (ENS) inspired by `<https://arxiv.org/pdf/1901.05555.pdf>`.
-    As the number of samples of a given class increases, the contribution of each newly added data point diminishes.
-    The idea is to measure a small neighboring region rather than a single data point.
+    Compute Effective Number of Samples (ENS) inspired by:
+    "Class-Balanced Loss Based on Effective Number of Samples" `<https://arxiv.org/pdf/1901.05555.pdf>`_.
     
     Args: 
         labels (array): Labels in the training dataset.
-        beta (float): Hyperparameters in [0,1[ representing the contribution of each new sample to the effective volume of all samples in the training dataset.
-                      Suggested values for beta: 0.9,0.99,0.999,0.9999.
+        beta (float): Hyperparameters in [0,1[. Suggested values: 0.9,0.99,0.999,0.9999.
 
-    
     Return: 
         class_weights (tensor): Inverse powered number of samples for each class. 
     """
@@ -234,10 +208,11 @@ def effective_number_samples(labels, beta = 0.9):
     assert beta < 1, "Beta takes its values between 0 and 1 excluded, please modify the value of beta."
     
     # Recover the number of samples for each class in the training dataset
-    count_label = get_class_distribution(labels)
-    class_count = [count_label[k] for k in np.unique(labels)]
+    count_labels = get_class_distribution(labels)
+    class_count = [count_labels[k] for k in np.unique(labels)]
     
     # Compute the corresponding weights
-    class_weights = (1 - beta) / (1 - np.power(beta, torch.tensor(class_count, dtype = torch.float))) 
+    class_weights = (1-beta) / (1-np.power(beta, torch.tensor(class_count, dtype=torch.float))) 
+    
     return class_weights
 
