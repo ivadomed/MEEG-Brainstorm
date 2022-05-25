777276f306e6960e7d265e7dc0b11bddddcb2d57
Theo Gnassounou - MEG Lab <theog@bic.mni.mcgill.ca>
2022-05-25 10:03:02 -0400

    add new metrics

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -14,18 +14,21 @@ def score(model, loader):
     y_pred_all, y_true_all = list(), list()
     with torch.no_grad():
         for batch_x, batch_y in loader:
-            batch_x = batch_x.to(device=device)
-            batch_y = batch_y.to(device=device)
-            _, output = model.forward(batch_x)
-            y_pred_all.append(torch.argmax(output, axis=1).cpu().numpy())
+            batch_x = batch_x.to(torch.float).to(device=device)
+            batch_y = batch_y.to(torch.float).to(device=device)
+            output, _ = model.forward(batch_x)
+            y_pred_all.append(output.cpu().numpy())
             y_true_all.append(batch_y.cpu().numpy())
 
     y_pred = np.concatenate(y_pred_all)
     y_true = np.concatenate(y_true_all)
 
-    acc = accuracy_score(y_true, y_pred)
-    f1 = f1_score(y_true, y_pred, average='weighted')
-    precision = precision_score(y_true, y_pred, average='weighted')
-    recall = recall_score(y_true, y_pred, average='weighted')
+    y_pred_binary = np.zeros(len(y_pred))
+    y_pred_binary[np.where(y_pred > 0.5)[0]] = 1
+
+    acc = accuracy_score(y_true, y_pred_binary)
+    f1 = f1_score(y_true, y_pred_binary, average='weighted')
+    precision = precision_score(y_true, y_pred_binary, average='weighted')
+    recall = recall_score(y_true, y_pred_binary, average='weighted')
 
     return acc, f1, precision, recall
