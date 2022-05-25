0203960826cb98000058a921af8643b87895c4c8
Theo Gnassounou - MEG Lab <theog@bic.mni.mcgill.ca>
2022-05-25 10:03:43 -0400

    fix the position of batch size in LSTM and Attention layer

--- utils/model.py
+++ utils/model.py
@@ -8,31 +8,31 @@ class fukumori2021RNN(nn.Module):
         self.input_size = input_size
         self.LSTM_1 = nn.LSTM(input_size=input_size,
                               hidden_size=8,
-                              num_layers=1)
+                              num_layers=1,
+                              batch_first=True)
         self.tanh = nn.Tanh()
         self.avgPool = nn.AvgPool1d(kernel_size=4, stride=4)
         self.selfattention = nn.MultiheadAttention(num_heads=1, embed_dim=8)
-        self.LSTM_2 = nn.LSTM(input_size=8, hidden_size=8, num_layers=1)
+        self.LSTM_2 = nn.LSTM(input_size=8, hidden_size=8, num_layers=1, batch_first=True)
         self.classifier = nn.Linear(256, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        h0 = torch.randn(1, 512, 8)
-        c0 = torch.randn(1, 512, 8)
+
         # First LSTM
-        x, (_, _) = self.LSTM_1(x) #, (h0, c0))
-        x = self.tanh(x)
+        x, (_, _) = self.LSTM_1(x)
         x = self.avgPool(x.transpose(1, 2))
         x = x.transpose(1, 2)
+        x = x.transpose(0, 1)
 
         # Self-attention Layer
         x_attention, attention_weights = self.selfattention(x, x, x)
-        x = x + x_attention
 
-        h0 = torch.randn(1, 128, 8)
-        c0 = torch.randn(1, 128, 8)
+        x = x + x_attention
+        x = x.transpose(0, 1)
+
         # Second LSTM
-        x, (_, _) = self.LSTM_2(x) #, (h0, c0))
+        x, (_, _) = self.LSTM_2(x)
         x = self.tanh(x)
         x = self.avgPool(x.transpose(1, 2))
         x = x.transpose(1, 2)
