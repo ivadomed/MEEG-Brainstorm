8f0adefc77891e58edd65ff3764643ef2eff2b78
AmbroiseOdonnat <ambroiseodonnattechnologie@gmail.com>
2022-06-01 15:58:55 -0400

    Create main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import pickle
+#!/usr/bin/env python
+
+"""
+This script is used to train models.
+
+Contributors: Ambroise Odonnat and Theo Gnassounou.
+"""
+
 import argparse
+import json
 import os
 import numpy as np
 import pandas as pd
@@ -12,17 +20,20 @@ from torch.optim import Adam
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import balanced_accuracy_score
 
-from utils.training import train
+from models.architectures import RNN_self_attention, STT
+from models.training import make_model
+from loader.dataloader import pad_loader
 from utils.evaluation import score
 from utils.data_edf import Data, SpikeDetectionDataset
-from utils.model import ClassificationBertMEEG, fukumori2021RNN
 from utils.loader import get_pad_dataloader
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def get_parser():
-    """Set parameters for the experiment."""
+
+    """ Set parameters for the experiment."""
+
     parser = argparse.ArgumentParser(
         "Spike detection", description="Spike detection using attention layer"
     )
@@ -42,26 +53,33 @@ def get_parser():
 # Experiment name
 parser = get_parser()
 args = parser.parse_args()
-# load data filtered
-expe_seed = args.seed
-method = args.method
-parameters = [0]
-batch_size = args.batch_size
-path_data = args.path_data
-sfreq = 100  # Sampling frequency
+path_root = args.path_root
+single_channel = args.single_channel
+path_config = args.path_config
 
-if method == 'Fukumori':
+# Recover config dictionnary
+with open(path_config) as f:
+    config = json.loads(f.read())
+
+# Recover params
+seed = config['seed']
+method = config['method']
+model = method['params']
+batch_size = config['batch_size']
+n_epochs = config['n_epochs']
+patience = config['patience']
+sample_frequence = config['sample_frequence']
+
+if method == 'RNN_self_attention':
     single_channel = True
 else:
     single_channel = False
 
-dataset = Data(path_data, 'spikeandwave', single_channel)
+dataset = Data(path_root, 'spikeandwave', single_channel)
 all_dataset = dataset.all_datasets()
 
-n_epochs = 100
-patience = 10
-
-assert method in ("Fukumori", "Ambroise")
+assert method in ("RNN_self_attention", "transformers_classification",
+                  "transformers_detection")
 
 results = []
 data = all_dataset[0]
@@ -83,8 +101,7 @@ for test_subject_id in subject_ids:
           'test on: {}'.format(test_subject_id,
                                val_subject_id))
 
-    
-
+    # Training dataloader
     train_data = []
     for id in train_subject_ids:
         sessions_trials = data[id]
@@ -96,17 +113,19 @@ for test_subject_id in subject_ids:
         for events in sessions_events:
             train_labels.append(events)
 
+    # Z-score normalization
     target_mean = np.mean([np.mean(data) for data in train_data])
     target_std = np.mean([np.std(data) for data in train_data])
     train_data = [np.expand_dims((data-target_mean) / target_std,
-                    axis=1)
-                    for data in train_data]
-    # Create training dataloader
-    loader_train = get_pad_dataloader(train_data,
-                                        train_labels,
-                                        batch_size,
-                                        True,
-                                        0)
+                                 axis=1)
+                  for data in train_data]
+    loader_train = pad_loader(train_data,
+                              train_labels,
+                              batch_size,
+                              True,
+                              0)
+
+    # Validation dataloader
     val_data = []
     sessions_trials = data[val_subject_id]
     for trials in sessions_trials:
@@ -116,20 +135,17 @@ for test_subject_id in subject_ids:
     for events in sessions_events:
         val_labels.append(events)
 
+    # Z-score normalization
     val_data = [np.expand_dims((data-target_mean) / target_std,
                 axis=1)
                 for data in val_data]
+    loader_val = pad_loader(val_data,
+                            val_labels,
+                            batch_size,
+                            False,
+                            0)
 
-    # Create val dataloader
-    loader_val = get_pad_dataloader(val_data,
-                                    val_labels,
-                                    batch_size,
-                                    False,
-                                    0)
-
-
-
-    # Create test dataloader
+    # Test dataloader
     test_data = []
     sessions_trials = data[test_subject_id]
     for trials in sessions_trials:
@@ -139,14 +155,15 @@ for test_subject_id in subject_ids:
     for events in sessions_events:
         test_labels.append(events)
 
+    # Z-score normalization
     test_data = [np.expand_dims((data-target_mean) / target_std,
-                axis=1)
-                for data in test_data]
+                                axis=1)
+                 for data in test_data]
     loader_test = get_pad_dataloader(test_data,
-                                        test_labels,
-                                        batch_size,
-                                        False,
-                                        0)
+                                     test_labels,
+                                     batch_size,
+                                     False,
+                                     0)
 
     input_size = loader_train.dataset[0][0].shape[1]
 
