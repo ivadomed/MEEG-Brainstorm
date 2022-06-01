671f087f1ed029c6cb688f930cec430bc44a38ce
AmbroiseOdonnat <ambroiseodonnattechnologie@gmail.com>
2022-06-01 09:43:07 -0400

    Create file to visualize feature maps

---
+++ utils/feature_maps_visualization.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+This script contains functions to visualize feature maps
+from convolutions layers. Inspired by:
+`<https://ravivaishnav20.medium.com/visualizing-feature-maps-using-pytorch-12a48cd1e573>`_.
+
+Usage: type "from feature_maps_visualization" import <class>" to use one class.
+
+Contributors: Ambroise Odonnat.
+"""
