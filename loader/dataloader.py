07a0f9701e0267e7f5ee97b291fe9600c41679aa
Théo <theo.gnassounou@gmail.com>
2022-06-01 17:10:33 -0400

    add weighted dataloader to balance the batch during training

--- loader/dataloader.py
+++ loader/dataloader.py
@@ -11,9 +11,23 @@ Contributors: Ambroise Odonnat and Theo Gnassounou.
 
 import torch
 
+import numpy as np
+
 from torch.utils.data import DataLoader, Dataset
+from torch.utils.data.sampler import WeightedRandomSampler
 
 from utils.utils_ import pad_tensor
+
+
+# TODO a mettre dans utils ?
+def class_imbalance_sampler(labels, n_sample):
+    class_count = torch.bincount(labels.squeeze())
+    class_weighting = 1. / class_count
+    sample_weights = class_weighting[labels]
+    sampler = WeightedRandomSampler(sample_weights,
+                                    n_sample,
+                                    replacement=False)
+    return sampler
 
 
 class PadCollate():
@@ -95,7 +109,6 @@ def multi_channel_loader(data,
         Input can be padded on the channel dimension
         with artifical zero channels to match highest
         number of channels in the batch.
-
     Args:
         data (list): List of array of trials of dimension
                      [n_trials x 1 x n_channels x n_time_points].
@@ -103,7 +116,6 @@ def multi_channel_loader(data,
         batch_size (float): Batch size.
         shuffle (bool): If True, shuffle batches in dataloader.
         num_workers (float): Number of loader worker processes.
-
     Returns:
         dataloader (Dataloader): Dataloader of batches.
     """
@@ -120,27 +132,64 @@ def multi_channel_loader(data,
     return loader
 
 
-def single_channel_loader(data,
-                          labels,
-                          batch_size,
-                          shuffle,
-                          num_workers):
+def weighted_loader(data,
+                    labels,
+                    batch_size,
+                    shuffle,
+                    num_workers):
 
-    """ Create dataloader for single-channel trials.
+    """ Create dataloader for multi-channel trials.
+        Input can be padded on the channel dimension
+        with artifical zero channels to match highest
+        number of channels in the batch.
+
     Args:
-        data (array): Array of trials of dimension
-                        [n_trials x n_time_points x 1].
-        labels (array): Array of labels of dimension [n_trials].
-        batch_size (int): Batch size.
+        data (list): List of array of trials of dimension
+                     [n_trials x 1 x n_channels x n_time_points].
+        labels (list): List of corresponding array of labels.
+        batch_size (float): Batch size.
         shuffle (bool): If True, shuffle batches in dataloader.
-        num_workers (int): Number of loader worker processed.
+        num_workers (float): Number of loader worker processes.
 
     Returns:
-        loader (Dataloader): Dataloader of batches.
+        dataloader (Dataloader): Dataloader of batches.
     """
 
-    dataset = SingleChannelDataset(data, labels)
-    loader = DataLoader(dataset=dataset, batch_size=batch_size,
-                        shuffle=shuffle, num_workers=num_workers)
+    # Get dataloader
+    datasets = []
+    n_ied_segments = []
+    label_distributions = []
+    for id in range(len(data)):
+        label_distribution = np.concatenate(labels[id])
+        label_distributions.append(label_distribution)
 
-    return loader
+        n_ied_segment = np.sum(label_distribution == 1)
+        n_ied_segments.append(n_ied_segment)
+
+        dataset = []
+        for n_trial in range(data[id].shape[0]):
+            dataset.append((data[id][n_trial], labels[id][n_trial]))
+        datasets.append(dataset)
+
+    # TODO mean or median or quartile ?
+    n_ied_segments_mean = n_ied_segments.mean()
+    n_ied_segments[n_ied_segments >= n_ied_segments_mean] = n_ied_segments_mean
+    loaders = []
+    for id in range(len(data)):
+
+        sampler = class_imbalance_sampler(label_distributions[id],
+                                          2*n_ied_segments[id])
+
+        loaders.append(DataLoader(dataset=datasets[id],
+                                  batch_size=batch_size,
+                                  shuffle=shuffle,
+                                  sampler=sampler,
+                                  num_workers=num_workers,
+                                  collate_fn=PadCollate(dim=1)))
+
+    # Sort the loaders in the desending order
+
+    argsort = np.argsort(n_ied_segments)
+    loaders = loaders[np.flipud(argsort)]
+
+    return loaders
