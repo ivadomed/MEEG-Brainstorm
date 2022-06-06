76b74303a2484f742f40f3fcd5ea503a59766898
AmbroiseOdonnat <ambroiseodonnattechnologie@gmail.com>
2022-06-06 18:32:10 -0400

    Update

--- models/training.py
+++ models/training.py
@@ -176,7 +176,9 @@ class make_model():
         y_pred = np.concatenate(all_preds)
         y_pred_binary = 1 * (y_pred > 0.5)
         y_true = np.concatenate(all_labels)
-
+        print('lab: ', y_true)
+        print('out', y_pred)
+        print('pred: ', y_pred_binary)
         # Recover mean loss and F1-score
         val_loss = np.mean(val_loss)
         perf = f1_score(y_true, y_pred_binary, average=average,
