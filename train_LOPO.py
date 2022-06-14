4a9b3255ae2b5a74497ec7dbda4eb71ef0e91fae
AmbroiseOdonnat <ambroiseodonnattechnologie@gmail.com>
2022-06-14 10:17:40 -0400

    Add subjects in validation LOPO

--- train_LOPO.py
+++ train_LOPO.py
@@ -114,7 +114,6 @@ for test_subject_id in subject_ids:
                                                    size))
     else:
         val_subject_ids = np.asarray([np.random.choice(train_subject_ids)])
-    print(size, val_subject_ids)
     for id in val_subject_ids:
         train_subject_ids = np.delete(train_subject_ids,
                                       np.where(train_subject_ids == id))
