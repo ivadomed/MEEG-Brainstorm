72147a1505207ef3c03bce138676e375d781b4ef
Ambroise Odonnat <ambroise.odonnat@eleves.enpc.fr>
2022-07-06 14:42:50 -0400

    Add seed to test robustness of model

--- train_LOPO.py
+++ train_LOPO.py
@@ -115,129 +115,129 @@ subject_ids = np.asarray(list(data.keys()))
 """ Each subject is chosen once as test set while the model is trained
     and validate on the remaining ones.
 """
-seed = 42
-np.random.seed(42)
-seed_list = [np.random.randint(0, 100) for _ in range(len(selected_subjects))]
-for i, test_subject_id in enumerate(subject_ids):
-    seed = seed_list[i]
-    # Labels are the spike events times
-    loader = Loader(data,
-                    labels,
-                    annotated_channels,
-                    single_channel=single_channel,
-                    batch_size=batch_size,
-                    num_workers=num_workers,
-                    subject_LOPO=test_subject_id,
-                    seed=seed,
-                    )
+for _ in range(5):
+    np.random.seed(42)
+    seed_list = [np.random.randint(0, 100) for _ in range(len(selected_subjects))]
+    for i, test_subject_id in enumerate(subject_ids):
+        seed = seed_list[i]
+        # Labels are the spike events times
+        loader = Loader(data,
+                        labels,
+                        annotated_channels,
+                        single_channel=single_channel,
+                        batch_size=batch_size,
+                        num_workers=num_workers,
+                        subject_LOPO=test_subject_id,
+                        seed=seed,
+                        )
 
-    train_loader, val_loader, test_loader, train_labels = loader.load()
+        train_loader, val_loader, test_loader, train_labels = loader.load()
 
-    # Define architecture
-    if method == "EEGNet":
-        n_time_points = len(data[subject_ids[0]][0][0][0])
-        architecture = EEGNet()
-        warmup = False
-    if method == "EEGNet_1D":
-        architecture = EEGNet_1D()
-        warmup = False
-    elif method == "GTN":
-        n_time_points = len(data[subject_ids[0]][0][0][0])
-        architecture = GTN(n_time_points=n_time_points)
-    elif method == "RNN_self_attention":
-        n_time_points = len(data[subject_ids[0]][0][0][0])
-        architecture = RNN_self_attention(n_time_points=n_time_points)
-        warmup = False
-    elif method == "STT":
-        n_time_points = len(data[subject_ids[0]][0][0][0])
-        architecture = STT(n_time_points=n_time_points)
-    elif method == "STTNet":
-        architecture = STTNet()
-    architecture.apply(reset_weights)
+        # Define architecture
+        if method == "EEGNet":
+            n_time_points = len(data[subject_ids[0]][0][0][0])
+            architecture = EEGNet()
+            warmup = False
+        if method == "EEGNet_1D":
+            architecture = EEGNet_1D()
+            warmup = False
+        elif method == "GTN":
+            n_time_points = len(data[subject_ids[0]][0][0][0])
+            architecture = GTN(n_time_points=n_time_points)
+        elif method == "RNN_self_attention":
+            n_time_points = len(data[subject_ids[0]][0][0][0])
+            architecture = RNN_self_attention(n_time_points=n_time_points)
+            warmup = False
+        elif method == "STT":
+            n_time_points = len(data[subject_ids[0]][0][0][0])
+            architecture = STT(n_time_points=n_time_points)
+        elif method == "STTNet":
+            architecture = STTNet()
+        architecture.apply(reset_weights)
 
-    # Define training loss
-    if weight_loss:
-        pos_weight = get_pos_weight(train_labels).to(device)
-        train_criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
-        train_criterion = train_criterion.to(device)
-    else:
-        train_criterion = criterion
-    train_criterion = get_criterion(train_criterion,
-                                    cost_sensitive,
-                                    lambd)
-    # Define optimizer
-    optimizer = Adam(architecture.parameters(), lr=lr,
-                     weight_decay=weight_decay)
-    warm_optimizer = NoamOpt(optimizer)
+        # Define training loss
+        if weight_loss:
+            pos_weight = get_pos_weight(train_labels).to(device)
+            train_criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+            train_criterion = train_criterion.to(device)
+        else:
+            train_criterion = criterion
+        train_criterion = get_criterion(train_criterion,
+                                        cost_sensitive,
+                                        lambd)
+        # Define optimizer
+        optimizer = Adam(architecture.parameters(), lr=lr,
+                        weight_decay=weight_decay)
+        warm_optimizer = NoamOpt(optimizer)
 
-    # Define training pipeline
-    architecture = architecture.to(device)
-    model = make_model(architecture,
-                       train_loader,
-                       val_loader,
-                       test_loader,
-                       optimizer,
-                       warmup,
-                       warm_optimizer,
-                       train_criterion,
-                       criterion,
-                       single_channel=single_channel,
-                       n_epochs=n_epochs,
-                       patience=patience)
+        # Define training pipeline
+        architecture = architecture.to(device)
+        model = make_model(architecture,
+                        train_loader,
+                        val_loader,
+                        test_loader,
+                        optimizer,
+                        warmup,
+                        warm_optimizer,
+                        train_criterion,
+                        criterion,
+                        single_channel=single_channel,
+                        n_epochs=n_epochs,
+                        patience=patience)
 
-    # Train Model
-    history = model.train()
+        # Train Model
+        history = model.train()
 
-    # Compute test performance and save it
-    acc, f1, precision, recall = model.score()
-    results.append(
-        {
-            "method": method,
-            "warmup": warmup,
-            "weight_loss": weight_loss,
-            "cost_sensitive": cost_sensitive,
-            "len_trials": len_trials,
-            "test_subject_id": test_subject_id,
-            "fold": seed,
-            "acc": acc,
-            "f1": f1,
-            "precision": precision,
-            "recall": recall
-        }
-    )
-    mean_acc += acc
-    mean_f1 += f1
-    mean_precision += precision
-    mean_recall += recall
-    steps += 1
-
-    if save:
-
-        # Save results file as csv
-        if not os.path.exists("../results"):
-            os.mkdir("../results")
-
-        results_path = (
-            "../results/csv_LOPO"
+        # Compute test performance and save it
+        acc, f1, precision, recall = model.score()
+        results.append(
+            {
+                "method": method,
+                "warmup": warmup,
+                "weight_loss": weight_loss,
+                "cost_sensitive": cost_sensitive,
+                "len_trials": len_trials,
+                "test_subject_id": test_subject_id,
+                "fold": seed,
+                "acc": acc,
+                "f1": f1,
+                "precision": precision,
+                "recall": recall
+            }
         )
-        if not os.path.exists(results_path):
-            os.mkdir(results_path)
+        mean_acc += acc
+        mean_f1 += f1
+        mean_precision += precision
+        mean_recall += recall
+        steps += 1
 
-        results_path = os.path.join(results_path,
-                                    "results_LOPO_spike_detection_{}"
-                                    "-subjects.csv".format(len(subject_ids))
-                                    )
-        df_results = pd.DataFrame(results)
-        results = []
+        if save:
 
-        if os.path.exists(results_path):
-            df_old_results = pd.read_csv(results_path)
-            df_results = pd.concat([df_old_results, df_results])
-        df_results.to_csv(results_path, index=False)
+            # Save results file as csv
+            if not os.path.exists("../results"):
+                os.mkdir("../results")
 
-print("Mean accuracy \t Mean F1-score \t Mean precision \t Mean recall")
-print("-" * 80)
-print(
-    f"{mean_acc/steps:0.4f} \t {mean_f1/steps:0.4f} \t"
-    f"{mean_precision/steps:0.4f} \t {mean_recall/steps:0.4f}\n"
-)
+            results_path = (
+                "../results/csv_LOPO"
+            )
+            if not os.path.exists(results_path):
+                os.mkdir(results_path)
+
+            results_path = os.path.join(results_path,
+                                        "results_LOPO_spike_detection_{}"
+                                        "-subjects.csv".format(len(subject_ids))
+                                        )
+            df_results = pd.DataFrame(results)
+            results = []
+
+            if os.path.exists(results_path):
+                df_old_results = pd.read_csv(results_path)
+                df_results = pd.concat([df_old_results, df_results])
+            df_results.to_csv(results_path, index=False)
+
+    print("Mean accuracy \t Mean F1-score \t Mean precision \t Mean recall")
+    print("-" * 80)
+    print(
+        f"{mean_acc/steps:0.4f} \t {mean_f1/steps:0.4f} \t"
+        f"{mean_precision/steps:0.4f} \t {mean_recall/steps:0.4f}\n"
+    )
